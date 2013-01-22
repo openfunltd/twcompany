626b7ce09064bf9f19895526f5e82eeedda8c3c7
Ronny Wang <ronnywang@gmail.com>
2013-01-22 15:58:34 +0800

    add ColumnGroup::getColumnId

--- webdata/models/ColumnGroup.php
+++ webdata/models/ColumnGroup.php
@@ -12,4 +12,22 @@ class ColumnGroup extends Pix_Table
 
         $this->addIndex('name', array('name'), 'unique');
     }
+
+    protected static $_column_groups = null;
+
+    public static function getColumnId($name)
+    {
+        if (is_null(self::$_column_groups)) {
+            self::$_column_groups = array();
+            foreach (ColumnGroup::search(1) as $group) {
+                self::$_column_groups[$group->name] = $group->id;
+            }
+        }
+
+        if (!array_key_exists($name, self::$_column_groups)) {
+            self::$_column_groups[$name] = ColumnGroup::insert(array('name' => $name))->id;
+        }
+
+        return self::$_column_groups[$name];
+    }
 }
