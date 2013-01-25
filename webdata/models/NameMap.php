9c9e979eba6fb8f25d85aad612d523ae92207715
Ronny Wang <ronnywang@gmail.com>
2013-01-25 14:07:56 +0800

    名字長度改成 128bytes

--- webdata/models/NameMap.php
+++ webdata/models/NameMap.php
@@ -8,7 +8,7 @@ class NameMap extends Pix_Table
         $this->_primary = 'id';
 
         $this->_columns['id'] = array('type' => 'int', 'auto_increment' => true);
-        $this->_columns['name'] = array('type' => 'varchar', 'size' => 32);
+        $this->_columns['name'] = array('type' => 'varchar', 'size' => 128);
         $this->_columns['unique'] = array('type' => 'tinyint', 'default' => 0);
 
         $this->addIndex('name_unique', array('name', 'unique'), 'unique');
