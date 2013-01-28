946524e50a4c02702828bb8af2e4d86abb0e032e
Ronny Wang <ronnywang@gmail.com>
2013-01-28 12:32:34 +0800

    CNS2UTF8::convert 可以把 cns 直接轉換成 utf8

---
+++ webdata/models/CNS2UTF8.php
@@ -0,0 +1,39 @@
+<?php
+
+class CNS2UTF8
+{
+    protected static $_maps = null;
+
+    public static function convert($page, $code)
+    {
+        $maps = self::_getMap();
+
+        if (!$unicode = $maps[intval($page)][strtolower($code)]) {
+            return '';
+        }
+
+        return html_entity_decode('&#x' . $unicode . ';');
+    }
+
+    protected static function _getMap()
+    {
+        if (!is_null(self::$_maps)) {
+            return self::$_maps;
+        }
+
+        self::$_maps = array();
+        foreach (glob(__DIR__ . '/../maps/cns2unicode/cns_unicode*.txt') as $file) {
+            $fp = fopen($file, 'r');
+            while (false !== ($line = fgets($fp))) {
+                if ($line[0] == '#') {
+                    continue;
+                }
+                list($cns, $unicode) = explode("\t", trim($line));
+                list($page, $code) = explode('-', $cns);
+
+                self::$_maps[intval($page)][strtolower($code)] = $unicode;
+            }
+        }
+        return self::$_maps;
+    }
+}
