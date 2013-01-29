0aee363761b47b187a6337d1e9fea9b53330129b
Ronny Wang <ronnywang@gmail.com>
2013-01-29 15:02:52 +0800

    把公司和商業登記分開

--- webdata/scripts/dump.php
+++ webdata/scripts/dump.php
@@ -1,4 +1,5 @@
 <?php
+ini_set('memory_limit', '256m');
 
 include(__DIR__ . '/../init.inc.php');
 Pix_Table::$_save_memory = true;
@@ -13,14 +14,19 @@ $delta = 10000000;
 for ($i = 0; $i * $delta < 99999999; $i ++) {
     $start = $i * $delta;
     $end = $start + $delta - 1;
-    $tmpname = tempnam('', '');
-    $file_name = 'files/' . str_pad($i * $delta, 8, '0', STR_PAD_LEFT) . '.json.gz';
-    $fp = gzopen($tmpname, 'w');
+    $tmpname1 = tempnam('', '');
+    $tmpname2 = tempnam('', '');
+    $file_name1 = 'files/' . str_pad($i * $delta, 8, '0', STR_PAD_LEFT) . '.json.gz';
+    $file_name2 = 'files/bussiness-' . str_pad($i * $delta, 8, '0', STR_PAD_LEFT) . '.json.gz';
+    $fp[1] = gzopen($tmpname1, 'w');
+    $fp[2] = gzopen($tmpname2, 'w');
+
     $unit_id = null;
     $unit = new StdClass;
+    $unit_types = Unit::search("`id` >= $start AND `id` <= $end")->toArray('type');
     foreach (UnitData::search("`id` >= $start AND `id` <= $end")->order("`id`, `column_id`")->volumemode(10000) as $unit_data) {
         if (!is_null($unit_id) and $unit_data->id != $unit_id) {
-            fwrite($fp, str_pad($unit_id, 8, '0', STR_PAD_LEFT) . ',' . json_encode($unit, JSON_UNESCAPED_UNICODE) . "\n");
+            fwrite($fp[$unit_types[$unit_id]], str_pad($unit_id, 8, '0', STR_PAD_LEFT) . ',' . json_encode($unit, JSON_UNESCAPED_UNICODE) . "\n");
             $unit = new StdClass;
         }
         $unit_id = $unit_data->id;
@@ -28,9 +34,12 @@ for ($i = 0; $i * $delta < 99999999; $i ++) {
         $unit->{$columns[$unit_data->column_id]} = json_decode($unit_data->value);
     }
     if (!is_null($unit_id)) {
-        fwrite($fp, str_pad($unit_id, 8, '0', STR_PAD_LEFT) . ',' . json_encode($unit, JSON_UNESCAPED_UNICODE) . "\n");
+        fwrite($fp[$unit_types[$unit_id]], str_pad($unit_id, 8, '0', STR_PAD_LEFT) . ',' . json_encode($unit, JSON_UNESCAPED_UNICODE) . "\n");
     }
-    fclose($fp);
-    DropboxLib::putFile($tmpname, $file_name);
-    unlink($tmpname);
+    fclose($fp[1]);
+    fclose($fp[2]);
+    DropboxLib::putFile($tmpname1, $file_name1);
+    DropboxLib::putFile($tmpname2, $file_name2);
+    unlink($tmpname1);
+    unlink($tmpname2);
 }
