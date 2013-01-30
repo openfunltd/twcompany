487f11f8ce3948d9bc0e40ca50c4582a3cb6f33d
Ronny Wang <ronnywang@gmail.com>
2013-01-30 16:18:09 +0800

    最後結束的也要產生

--- webdata/scripts/dump-index.php
+++ webdata/scripts/dump-index.php
@@ -13,7 +13,7 @@ $tmpname = tempnam('', '');
 $file_name = 'index.csv.gz';
 $fp = gzopen($tmpname, 'w');
 fwrite($fp, '#id,type,name' . PHP_EOL);
-$units = array();
+$units = $unit_names = $types = array();
 $type_names = array(1 => '公司', 2 => '商業登記', 4 => '教育部', 99 => '其他');
 foreach (Unit::search(1)->order('id')->volumemode(10000) as $unit) {
     $units[$unit->type][] = $unit->id;
@@ -46,7 +46,36 @@ foreach (Unit::search(1)->order('id')->volumemode(10000) as $unit) {
             }
             fputcsv($fp, $rows);
         }
+        $units = $unit_names = $types = array();
     }
+}
+if (count($unit_names)) {
+    // 1 - 公司
+    foreach (UnitData::search(array('column_id' => 2))->searchIn('id', $units[1]) as $unitdata) {
+        $unit_names[$unitdata->id] = $unitdata->value;
+    }
+    // 2 - 商業登記
+    foreach (UnitData::search(array('column_id' => 33))->searchIn('id', $units[2]) as $unitdata) {
+        $unit_names[$unitdata->id] = $unitdata->value;
+    }
+    // 4 - 教育部, 99 - 其他
+    foreach (UnitData::search(array('column_id' => 43))->searchIn('id', array_merge($units[4], $units[99])) as $unitdata) {
+        $unit_names[$unitdata->id] = $unitdata->value;
+    }
+
+    foreach ($unit_names as $id => $value) {
+        $rows = array();
+        $rows[] = str_pad($id, 8, '0', STR_PAD_LEFT);
+        $rows[] = $type_names[$types[$id]];
+        $v = json_decode($value);
+        if (is_array($v)) {
+            $rows[] = $v[0];
+        } else {
+            $rows[] = strval($v);
+        }
+        fputcsv($fp, $rows);
+    }
+    $units = $unit_names = $types = array();
 }
 fclose($fp);
 DropboxLib::putFile($tmpname, $file_name);
