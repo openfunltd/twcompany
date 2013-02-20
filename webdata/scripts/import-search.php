8f695e27f8d50296df32a4e027db89a6232a13ad
Ronny Wang <ronnywang@gmail.com>
2013-02-20 12:11:15 +0800

    add include init.inc.php

--- webdata/scripts/import-search.php
+++ webdata/scripts/import-search.php
@@ -1,11 +1,12 @@
 #!/usr/bin/env php
 <?php
-
+include(__DIR__ . '/../init.inc.php');
 $fp = gzopen($_SERVER['argv'][1], 'r');
 $curl = curl_init();
 while ($line = fgets($fp)) {
     list($id, $data) = explode(',', $line, 2);
-    curl_setopt($curl, CURLOPT_URL, getenv('SEARCH_URL') . '/company/company/' . $id);
+    $url = getenv('SEARCH_URL') . '/company/company/' . $id;
+    curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_HEADER, 0);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
