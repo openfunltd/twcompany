4f79529aa5691ffbb1dc944ce9b2cc52d38f3fad
Ronny Wang <ronnywang@gmail.com>
2013-02-22 17:48:13 +0800

    add import-keyword script

---
+++ webdata/scripts/import-keyword.php
@@ -0,0 +1,12 @@
+<?php
+
+include(__DIR__ . '/init.inc.php');
+Pix_Table::$_save_memory = true;
+$ids = (Updater::searchByKeyword($_SERVER['argv'][1]));
+foreach ($ids as $id) {
+    if (Unit::find($id)) {
+        continue;
+    }
+    Updater::update($id);
+    Unit::find($id)->updateSearch();
+}
