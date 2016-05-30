1a24d4a82c5ba87680838e427aa59d0fd93e98c3
Ronny Wang <ronnywang@gmail.com>
2016-05-30 15:28:57 +0800

    加上搜尋分公司的 API

--- webdata/controllers/ApiController.php
+++ webdata/controllers/ApiController.php
@@ -75,4 +75,20 @@ class ApiController extends Pix_Controller
         $ret->found = $search_ret->hits->total;
         return $this->jsonp($ret, strval($_GET['callback']));
     }
+
+    public function branchAction()
+    {
+        $page = intval($_GET['page']) ?: 1;
+        $search_ret = SearchLib::searchCompaniesByParent($_GET['q'], $page);
+        $ret = new StdClass;
+        $data = array();
+        foreach ($search_ret->hits->hits as $hit) {
+            $hit->_source->{'統一編號'} = $hit->_id;
+            $data[] = $hit->_source;
+        }
+
+        $ret->data = $data;
+        $ret->found = $search_ret->hits->total;
+        return $this->jsonp($ret, strval($_GET['callback']));
+    }
 }
