73baf88f4bfc51cfb9c8321f3d02442fda4dd47e
Ronny Wang <ronnywang@gmail.com>
2017-11-16 10:16:05 +0800

    更新搜尋的位置

--- webdata/models/SearchLib.php
+++ webdata/models/SearchLib.php
@@ -124,7 +124,7 @@ class SearchLib
                     'query' => array("query_string" => array('query' => $q )),
                 )) . "\n";
             }
-            $url = getenv('SEARCH_URL') . '/twcompany/name_map/_msearch';
+            $url = getenv('SEARCH_URL') . '/name_map/_msearch';
             $curl = curl_init();
             curl_setopt($curl, CURLOPT_URL, $url);
             curl_setopt($curl, CURLOPT_HEADER, 0);
